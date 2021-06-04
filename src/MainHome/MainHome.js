f09c64a4e7b9591ed01a1004ef50d4666f176c62
Ashik Joy <79218049+ashikjoy99@users.noreply.github.com>
2021-06-04 21:58:34 +0530

    adding search

--- src/MainHome/MainHome.js
+++ src/MainHome/MainHome.js
@@ -4,22 +4,29 @@ import './MainHome.css'
 
 const MainHome = () => {
 
-    const [state, setState] = useState('')
-
+    const [state, setState] = useState('');
+    const [search, setSearch] = useState('')
     useEffect(() => {
         fetch(' http://54.253.29.55/api/stores')
         .then(response=> response.json())
         .then(names=>setState(names.data))
     })
+
+    const filtered = state && state.filter(item=>{
+        return item.name.toLowerCase().includes(search.toLowerCase());
+    })
+
+    
+
     return (
         <div className='main-home'>
             <div className='searchbar'>
                 <h2>Our Stores</h2>
-                <input className='input' placeholder='Search Store'/>
+                <input onChange={(event) => setSearch(event.target.value)} className='input' placeholder='Search Store'/>
             </div>
             <div>
             {
-              state &&  state.slice(0,10).map((item,index)=>{
+              state &&  filtered.map((item,index)=>{
                   return  (<Stores key={index} name={item.name} address={item.address}/>)
                 })
             }
